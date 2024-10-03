e04af0e6a6bfe3e622c81c95601a031901d50ffa
Austin Tsow <atsow@icloud.com>
2024-10-03 15:55:09 -0700

    simple helper functions

--- app/src/main/java/memorygame/HelperFunctions.kt
+++ app/src/main/java/memorygame/HelperFunctions.kt
@@ -1,4 +1,16 @@
 package memorygame
 
 class HelperFunctions {
+    fun shuffleCards(cards:List<String>): List <String> {
+        return cards.shuffled()
+    }
+    fun isMatch(card1: String, card2: String): Boolean {
+        return card1 == card2
+    }
+    fun generateCardPairs(cardTypes: List<String>): List<String> {
+        return cardTypes.flatMap { listOf(it, it) }
+    }
+    fun isGameComplete(matchedCards: Set<String>, totalPairs: Int): Boolean {
+        return matchedCards.size == totalPairs
+    }
 }

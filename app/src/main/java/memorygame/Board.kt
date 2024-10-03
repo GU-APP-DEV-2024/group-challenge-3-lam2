40479a229f146def961afb65e1ba7ec90c808137
mtala1 <114954086+mtala1@users.noreply.github.com>
2024-10-03 15:46:34 -0700

    made board array

--- app/src/main/java/memorygame/Board.kt
+++ app/src/main/java/memorygame/Board.kt
@@ -3,12 +3,11 @@ package memorygame
 class Board( ) {
     //Constructor to create board of different sizes.
     // Assign
-    private val cards: Array<Card> = arrayOf<Card>(Card('c', "a"),
-        Card('h', "10"),
-        Card('h', "10"),
-        Card('d', "3"),
-        Card('c', "a"),
-        Card('d', "3"))
+    private var cards: Array<Card> = emptyArray<Card>()
+
+    init {
+
+    }
 
     fun getValueAt(index:Int): Card {
         return cards[index]
